3833b2e4c4b27bf71c4c1725cc074e52068711eb
Charles Bentein <c.bentein@rubika-edu.com>
2020-02-20 12:18:56 +0100

    Willing to test lighting conditions of torches
    to do : simple activation / activation after a certain one / put out the previous torch if not activated in time

---
+++ Action - Aventure/Assets/Scripts/Puzzles/Torches/Torch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch : MonoBehaviour
+{
+    private bool isLit;
+    private SpriteRenderer flame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flame = gameObject.GetComponent<SpriteRenderer>();
+        flame.enabled = false;
+        isLit = false;
+    }
+
+    protected virtual void Light()
+    {
+        if (!isLit)
+        {
+            flame.enabled = true;
+            isLit = true;
+        }
+    }
+
+    public void PutOut()
+    {
+        if (isLit)
+        {
+            flame.enabled = false;
+            isLit = false;
+        }
+    }   
+}
