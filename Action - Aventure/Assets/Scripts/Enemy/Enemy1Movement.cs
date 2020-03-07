d3ec8329c8f5daad9d696e6df00c0681c147093a
Xanthin <x.paris@rubika-edu.com>
2020-03-07 16:46:34 +0100

    Path Enemy + TrackLight
    The enemy follow his path while he don't find light. When he keeps the light, the enemy goes at his position and reprend son path habituel

---
+++ Action - Aventure/Assets/Scripts/Enemy/Enemy1Movement.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1Movement : MonoBehaviour
+{
+    //XP The enemy moves according positions selected by Us.
+
+    //Start Statement
+
+    public float speed = 10f;
+    [SerializeField]
+
+    private Transform target;
+
+    private int waypointsIndex = 0;
+
+    [SerializeField]
+    private bool lightFound = false;
+
+    //End Statement
+
+
+    void Start()
+    {
+        target = GetWaypoints.points[0];    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //When the enemu don't found light, he keeps following points.
+        if(lightFound == false)
+        {
+            Vector3 dir = target.position - transform.position;
+            transform.Translate(dir.normalized * speed * Time.deltaTime);
+
+            //Check if the position is approximatively the good one
+            if (Vector2.Distance(transform.position, target.position) <= 0.05)
+            {
+                //go to next waypoints
+                GetNextWaypoint();
+            }
+
+            if (waypointsIndex >= GetWaypoints.points.Length - 1)
+            {
+                target = GetWaypoints.points[0];
+                waypointsIndex = 0;
+            }
+        }
+
+       
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (lightFound == false)
+            {
+                lightFound = true;
+
+            }
+            else {
+                lightFound = false;
+            }
+
+           
+        }
+        
+    }
+    private void GetNextWaypoint()
+    {  
+        waypointsIndex++;
+        //fix the new waypoints positions (= new target)
+        target = GetWaypoints.points[waypointsIndex];
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+       
+        Debug.Log(lightFound);
+        target = collision.transform;
+        Vector3 dir = target.position - transform.position;
+
+
+            if (Vector2.Distance(transform.position, target.position) <= 0.2)
+        {
+            collision.gameObject.SetActive(false);
+            lightFound = false;
+        }
+    }
+}
