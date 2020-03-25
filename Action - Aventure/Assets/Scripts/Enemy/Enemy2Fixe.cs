465d4578f8e04db29f7b24528f65ce9d60630e37
Xanthin <x.paris@rubika-edu.com>
2020-03-25 17:56:36 +0100

    behaviour Ennemie
    Ajout Idle Fixe et Idle Mobile - Starting attaque

---
+++ Action - Aventure/Assets/Scripts/Enemy/Enemy2Fixe.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy2Fixe : MonoBehaviour
+{
+    //Target
+    public GameObject waypointFixe;
+    private Transform target;
+    public GameObject player;
+
+    //Velocity
+    public Rigidbody2D rb;
+    public float speed = 10f;
+
+    //Distance
+    public float enterPlayerArea = 2f;
+
+    //Camera
+    public CameraShake cameraShake;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = waypointFixe.transform;
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 dir = target.position - transform.position;
+
+        if (Vector2.Distance(transform.position, player.transform.position) <= enterPlayerArea)
+        {
+           
+            Debug.Log("playerFound");
+            target = player.transform;
+            StartCoroutine(cameraShake.Shake(.05f, .05f));
+          
+
+        } else  
+
+        if (Vector2.Distance(transform.position, player.transform.position) >= enterPlayerArea)
+        {
+            
+            Debug.Log("Player Outside");
+            target = waypointFixe.transform;
+           
+        }
+         rb.velocity = dir.normalized * speed * Time.deltaTime;
+
+        if (Vector2.Distance(transform.position, target.position) <= 0.1f)
+            {
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+
+    }
+
+}
--- Action - Aventure/Assets/Scripts/Enemy/Enemy2Movement.cs
+++ Action - Aventure/Assets/Scripts/Enemy/Enemy2Movement.cs
@@ -4,38 +4,64 @@ using UnityEngine;
 
 public class Enemy2Movement : MonoBehaviour
 {
-    public GameObject player;
 
-
+    //Target
     public GameObject waypoints;
     private Transform target;
     private int waypointIndex = 0;
+    public GameObject player;
 
+    //Velocity
     public Rigidbody2D rb;
-
     public float speed = 10f;
 
-    public float distanceMax = 4f;
+    //distance
+
     public float enterPlayerArea = 2f;
-    public float distanceToSwitchTargetWaypoints = 0.2f;
+    public float distanceToSwitchTargetWaypoints = 0.5f;
+
+    //Clock
+    public bool clockOneEnded;
+    public bool clockTwoEnded;
+    public bool playerNotFound;
+
+    //Camera
+    public CameraShake cameraShake;
 
 
     private void Start()
     {
         target = waypoints.GetComponent<GetWaypoints>().points[0];
+
+        clockOneEnded = true;
+      
+        playerNotFound = true;
+
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     private void Update()
     {
-        if(Vector2.Distance(transform.position, player.transform.position) <= enterPlayerArea)
+        Vector2 dir = target.position - transform.position;
+
+        if (Vector2.Distance(transform.position, player.transform.position) <= enterPlayerArea)
         {
+            playerNotFound = false;
             Debug.Log("playerFound");
             target = player.transform;
+            StartCoroutine(cameraShake.Shake(.05f, .05f));
+
+            rb.velocity = dir.normalized * speed * Time.deltaTime;
+
         }
-        if (Vector2.Distance(transform.position, player.transform.position) >= distanceMax)
+        if (Vector2.Distance(transform.position, player.transform.position) >= enterPlayerArea)
         {
+            playerNotFound = true;
             Debug.Log("Player Outside");
             target = waypoints.GetComponent<GetWaypoints>().points[waypointIndex];
+            
+            
 
             if (Vector2.Distance(transform.position, target.position) <= distanceToSwitchTargetWaypoints)
             {
@@ -43,11 +69,26 @@ public class Enemy2Movement : MonoBehaviour
             }
         }
 
+        
 
-        Vector2 dir = target.position - transform.position;
-        rb.velocity = dir.normalized * speed * Time.deltaTime;
+        
+        if (clockTwoEnded == true)
+        {
+            rb.velocity = dir.normalized * speed * Time.deltaTime;
+            clockOne();
 
+
+        }
+        //Start clocktwo
+        if (clockOneEnded == true)
+        {
+           
+            rb.velocity = new Vector3(0, 0, 0);
+            StartCoroutine(cameraShake.Shake(.05f, .05f));
+            clockTwo();
        
+
+        }
 
     }
 
@@ -59,8 +100,36 @@ public class Enemy2Movement : MonoBehaviour
         {
             waypointIndex = 0;
         }
+
         target = waypoints.GetComponent<GetWaypoints>().points[waypointIndex];
 
+    }
+
+    private void clockOne()
+    {
+        StartCoroutine("Clock1");
+    }
+
+    private void clockTwo()
+    {
+        StartCoroutine("Clock2");
+    }
+
+
+    IEnumerator Clock1()
+    {
+            clockTwoEnded = false;
+            yield return new WaitForSecondsRealtime(0.5f);
+            clockOneEnded = true;
+            Debug.Log("clockOne Move");
+    }
+    //Stop Mouvement
+    IEnumerator Clock2()
+    {
+            clockOneEnded = false;
+            yield return new WaitForSecondsRealtime(0.5f);
+            clockTwoEnded = true;
+            Debug.Log("clockTwo Move");
     }
 }
 
