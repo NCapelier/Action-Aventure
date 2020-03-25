291660ed6be4dc84126ad47c009acdb8221ff468
Xanthin <x.paris@rubika-edu.com>
2020-03-25 23:01:15 +0100

    Attaque Ennemie 2
    Encore quelques bugs sur l'ennemi mobile / Besoin de faire le retour en s'accader

--- Action - Aventure/Assets/Scripts/Enemy/Enemy2Fixe.cs
+++ Action - Aventure/Assets/Scripts/Enemy/Enemy2Fixe.cs
@@ -8,13 +8,31 @@ public class Enemy2Fixe : MonoBehaviour
     public GameObject waypointFixe;
     private Transform target;
     public GameObject player;
+    public int direction;
+    public float ennemiRangeAttack;
+    public int attackDirection;
 
     //Velocity
     public Rigidbody2D rb;
     public float speed = 10f;
+    public Vector3 move;
+
 
     //Distance
-    public float enterPlayerArea = 2f;
+    public float enterPlayerArea;
+
+    //Attaque
+    public float attackSpeed;
+    public bool canAttack;
+    public float warningTime;
+    public bool isAttacking;
+    public float immobilizationTime;
+
+    //Zone de dégats
+    public GameObject LeftAttack;
+    public GameObject RightAttack;
+    public GameObject UpAttack;
+    public GameObject DownAttack;
 
     //Camera
     public CameraShake cameraShake;
@@ -25,20 +43,32 @@ public class Enemy2Fixe : MonoBehaviour
         target = waypointFixe.transform;
 
         rb = GetComponent<Rigidbody2D>();
-    }
+
+        canAttack = true;
+
+
+        LeftAttack.SetActive(false);
+        RightAttack.SetActive(false);
+        UpAttack.SetActive(false);
+        DownAttack.SetActive(false);
+}
 
     // Update is called once per frame
     void Update()
     {
         Vector2 dir = target.position - transform.position;
 
+        Direction();
+
         if (Vector2.Distance(transform.position, player.transform.position) <= enterPlayerArea)
         {
            
             Debug.Log("playerFound");
             target = player.transform;
-            StartCoroutine(cameraShake.Shake(.05f, .05f));
-          
+            if (Vector2.Distance(transform.position, player.transform.position) >= ennemiRangeAttack)
+            {
+                StartCoroutine(cameraShake.Shake(.05f, .05f));
+            }
 
         } else  
 
@@ -49,13 +79,101 @@ public class Enemy2Fixe : MonoBehaviour
             target = waypointFixe.transform;
            
         }
-         rb.velocity = dir.normalized * speed * Time.deltaTime;
 
-        if (Vector2.Distance(transform.position, target.position) <= 0.1f)
-            {
+        
+
+        rb.velocity = dir.normalized * speed * Time.deltaTime;
+
+        if (Vector2.Distance(transform.position, player.transform.position) <= ennemiRangeAttack)
+        {
             rb.velocity = new Vector3(0, 0, 0);
+            StartCoroutine(Attack());
+
+            if(isAttacking == true)
+            {
+                rb.velocity = new Vector3(0, 0, 0);
+            }
         }
 
+        
+        Debug.Log(direction);
+    }
+    void Direction()
+    {
+        move = (player.transform.position - transform.position).normalized;
+
+        if (move != Vector3.zero)
+        {
+            if (move.y >= Mathf.Sqrt(2) / 2 && move.x <= Mathf.Sqrt(2) / 2 && move.x >= -Mathf.Sqrt(2) / 2)
+            {
+                direction = 0;        //up
+            }
+            else if (move.y <= -Mathf.Sqrt(2) / 2 && move.x <= Mathf.Sqrt(2) / 2 && move.x >= -Mathf.Sqrt(2) / 2)
+            {
+                direction = 2;        //down               
+            }
+            else if (move.x >= Mathf.Sqrt(2) / 2 && move.y <= Mathf.Sqrt(2) / 2 && move.y >= -Mathf.Sqrt(2) / 2)
+            {
+                direction = 1;        //right                
+            }
+            else if (move.x <= -Mathf.Sqrt(2) / 2 && move.y <= Mathf.Sqrt(2) / 2 && move.y >= -Mathf.Sqrt(2) / 2)
+            {
+                direction = 3;        //left 
+            }
+        }
+    }
+
+
+    IEnumerator Attack()
+    {
+        if (canAttack == true)
+        {
+
+            canAttack = false;
+            isAttacking = true;
+
+            rb.velocity = new Vector3(0, 0, 0);
+            yield return new WaitForSeconds(warningTime);
+
+            switch (direction)
+            {
+                case 0:
+                    Debug.Log("Attaque Haute");
+                    UpAttack.SetActive(true);
+                    break;
+                case 1:
+                    Debug.Log("Attaque Droite");
+                    RightAttack.SetActive(true);
+                    break;
+                case 2:
+                    Debug.Log("Attaque En bas");
+                    DownAttack.SetActive(true);
+                    break;
+                case 3:
+                    Debug.Log("Attaque Gauche");
+                    LeftAttack.SetActive(true);
+                    break;
+                default:
+                    break;
+            }
+
+            yield return new WaitForSeconds(0.001f);
+
+           
+            yield return new WaitForSeconds(immobilizationTime);
+
+            LeftAttack.SetActive(false);
+            RightAttack.SetActive(false);
+            UpAttack.SetActive(false);
+            DownAttack.SetActive(false);
+            isAttacking = false;
+
+            yield return new WaitForSeconds(attackSpeed);
+
+            canAttack = true;
+
+        }
     }
 
 }
+
--- Action - Aventure/Assets/Scripts/Enemy/Enemy2Movement.cs
+++ Action - Aventure/Assets/Scripts/Enemy/Enemy2Movement.cs
@@ -10,6 +10,8 @@ public class Enemy2Movement : MonoBehaviour
     private Transform target;
     private int waypointIndex = 0;
     public GameObject player;
+    public int attackDirection;
+    public int direction;
 
     //Velocity
     public Rigidbody2D rb;
@@ -19,6 +21,7 @@ public class Enemy2Movement : MonoBehaviour
 
     public float enterPlayerArea = 2f;
     public float distanceToSwitchTargetWaypoints = 0.5f;
+    public float ennemiRangeAttack;
 
     //Clock
     public bool clockOneEnded;
@@ -27,6 +30,21 @@ public class Enemy2Movement : MonoBehaviour
 
     //Camera
     public CameraShake cameraShake;
+
+     //Attaque
+    public float attackSpeed;
+    public bool canAttack;
+    public float warningTime;
+    public bool isAttacking;
+    public float immobilizationTime;
+    public Vector3 move;
+
+    //Zone de dégats
+    public GameObject LeftAttack;
+    public GameObject RightAttack;
+    public GameObject UpAttack;
+    public GameObject DownAttack;
+
 
 
     private void Start()
@@ -39,20 +57,33 @@ public class Enemy2Movement : MonoBehaviour
 
         rb = GetComponent<Rigidbody2D>();
 
+        canAttack = true;
+
+
+        LeftAttack.SetActive(false);
+        RightAttack.SetActive(false);
+        UpAttack.SetActive(false);
+        DownAttack.SetActive(false);
+
     }
 
     private void Update()
     {
         Vector2 dir = target.position - transform.position;
 
+        Direction();
+       
         if (Vector2.Distance(transform.position, player.transform.position) <= enterPlayerArea)
         {
+            rb.velocity = dir.normalized * speed * Time.deltaTime;
             playerNotFound = false;
             Debug.Log("playerFound");
             target = player.transform;
-            StartCoroutine(cameraShake.Shake(.05f, .05f));
-
-            rb.velocity = dir.normalized * speed * Time.deltaTime;
+            if (Vector2.Distance(transform.position, player.transform.position) >= ennemiRangeAttack)
+            {
+                StartCoroutine(cameraShake.Shake(.05f, .05f));
+            }
+            
 
         }
         if (Vector2.Distance(transform.position, player.transform.position) >= enterPlayerArea)
@@ -84,10 +115,24 @@ public class Enemy2Movement : MonoBehaviour
         {
            
             rb.velocity = new Vector3(0, 0, 0);
-            StartCoroutine(cameraShake.Shake(.05f, .05f));
+            if (Vector2.Distance(transform.position, player.transform.position) >= ennemiRangeAttack)
+            {
+                StartCoroutine(cameraShake.Shake(.05f, .05f));
+            }
             clockTwo();
        
 
+        }
+
+        if (Vector2.Distance(transform.position, player.transform.position) <= ennemiRangeAttack)
+        {
+            rb.velocity = new Vector3(0, 0, 0);
+            StartCoroutine(Attack());
+
+            if (isAttacking == true)
+            {
+                rb.velocity = new Vector3(0, 0, 0);
+            }
         }
 
     }
@@ -130,6 +175,82 @@ public class Enemy2Movement : MonoBehaviour
             yield return new WaitForSecondsRealtime(0.5f);
             clockTwoEnded = true;
             Debug.Log("clockTwo Move");
+    }
+
+    void Direction()
+    {
+        move = (player.transform.position - transform.position).normalized;
+
+        if (move != Vector3.zero)
+        {
+            if (move.y >= Mathf.Sqrt(2) / 2 && move.x <= Mathf.Sqrt(2) / 2 && move.x >= -Mathf.Sqrt(2) / 2)
+            {
+                direction = 0;        //up
+            }
+            else if (move.y <= -Mathf.Sqrt(2) / 2 && move.x <= Mathf.Sqrt(2) / 2 && move.x >= -Mathf.Sqrt(2) / 2)
+            {
+                direction = 2;        //down               
+            }
+            else if (move.x >= Mathf.Sqrt(2) / 2 && move.y <= Mathf.Sqrt(2) / 2 && move.y >= -Mathf.Sqrt(2) / 2)
+            {
+                direction = 1;        //right                
+            }
+            else if (move.x <= -Mathf.Sqrt(2) / 2 && move.y <= Mathf.Sqrt(2) / 2 && move.y >= -Mathf.Sqrt(2) / 2)
+            {
+                direction = 3;        //left 
+            }
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        if (canAttack == true)
+        {
+
+            canAttack = false;
+            isAttacking = true;
+
+            rb.velocity = new Vector3(0, 0, 0);
+            yield return new WaitForSeconds(warningTime);
+
+            switch (direction)
+            {
+                case 0:
+                    Debug.Log("Attaque Haute");
+                    UpAttack.SetActive(true);
+                    break;
+                case 1:
+                    Debug.Log("Attaque Droite");
+                    RightAttack.SetActive(true);
+                    break;
+                case 2:
+                    Debug.Log("Attaque En bas");
+                    DownAttack.SetActive(true);
+                    break;
+                case 3:
+                    Debug.Log("Attaque Gauche");
+                    LeftAttack.SetActive(true);
+                    break;
+                default:
+                    break;
+            }
+
+            yield return new WaitForSeconds(0.001f);
+
+
+            yield return new WaitForSeconds(immobilizationTime);
+
+            LeftAttack.SetActive(false);
+            RightAttack.SetActive(false);
+            UpAttack.SetActive(false);
+            DownAttack.SetActive(false);
+            isAttacking = false;
+
+            yield return new WaitForSeconds(attackSpeed);
+
+            canAttack = true;
+
+        }
     }
 }
 
