023d8ef156c8d469e8a4cf3e73be12c69439aa0c
Charles Bentein <c.bentein@rubika-edu.com>
2020-03-30 19:22:59 +0200

    Started TorchPursuit Test ; Circle Collider not detecting object

--- Action - Aventure/Assets/Scripts/Enemy/Enemy1Controller.cs
+++ Action - Aventure/Assets/Scripts/Enemy/Enemy1Controller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Player;
 using Lantern;
+using Puzzle;
 
 namespace Enemy
 {
@@ -30,6 +31,13 @@ namespace Enemy
         [Range(0f, 8f)]
         [SerializeField] float lightDetectExtra = 4f;
 
+        [Range(0f, 15f)]
+        [SerializeField] float torchDetectRange = 5f;
+
+        CircleCollider2D eyeForTorches;
+        GameObject targetTorch = null;
+        Torch torchLight = null;
+
         bool focusingPlayer = false;
 
         #endregion
@@ -42,6 +50,9 @@ namespace Enemy
         void Start()
         {
             EnemyRb = GetComponentInParent<Rigidbody2D>();
+            eyeForTorches = GetComponent<CircleCollider2D>();
+            eyeForTorches.radius = 0.01f;
+            StartCoroutine(RedetectTorch());
         }
         
         void Update()
@@ -49,7 +60,21 @@ namespace Enemy
             MoveToPlayer();
             MoveToLight();
         }
-        
+
+        private void OnTriggerStay2D(Collider2D collision)
+        {
+            if(collision.gameObject.tag == "Torch")
+            {
+                Debug.Log("Torch Detected");
+                torchLight = collision.gameObject.GetComponentInParent<Torch>();
+
+                if (torchLight.isLit)
+                {
+                    targetTorch = collision.gameObject;
+                    eyeForTorches.enabled = false;
+                }
+            }
+        }
         /// <summary>
         /// Moves this enemy towards the player until it's close enouth depending on contactDistance
         /// </summary>
@@ -82,6 +107,38 @@ namespace Enemy
                 {
                     EnemyRb.velocity = Vector2.zero;
                 }
+            }
+            
+        }
+
+        void MoveToTorch()
+        {
+            if(targetTorch != null)
+            {
+                if (!Vector2.Distance(targetTorch.transform.position, transform.parent.transform.position).isBetween(contactDistance, false, torchDetectRange, true) || !torchLight.isLit)
+                {
+                    targetTorch = null;
+                    torchLight = null;
+                    StartCoroutine(RedetectTorch());
+                    EnemyRb.velocity = Vector2.zero;
+                }
+
+                if (targetTorch != null && !focusingPlayer)
+                {
+                    EnemyRb.velocity = (targetTorch.transform.position - transform.parent.transform.position).normalized * moveSpeed * Time.deltaTime;
+                }
+            }
+        }
+
+        IEnumerator RedetectTorch()
+        {
+            eyeForTorches.radius = torchDetectRange * 0.01f;
+            eyeForTorches.enabled = true;
+
+            while (eyeForTorches.radius < torchDetectRange && targetTorch == null)
+            {
+                yield return new WaitForSeconds(0.0005f);
+                eyeForTorches.radius += torchDetectRange * 0.01f;
             }
             
         }
