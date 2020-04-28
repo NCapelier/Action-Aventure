344b0f3976fc3a121a329c427e5c00db0276d1a1
Nicolas Capelier <capelier.nicolas@gmail.com>
2020-04-28 17:21:46 +0200

    Added Dialog System

---
+++ Action - Aventure/Assets/Scriptable Objects/Conversation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Dialog
+{
+    [System.Serializable] public struct Line
+    {
+        public int leftPortraitIndex, rightPortraitIndex;
+
+        [TextArea(2, 5)] public string text;
+    }
+
+   [CreateAssetMenu(fileName = "New Conversation", menuName = "Dialog/Conversation")]
+    public class Conversation : ScriptableObject
+    {
+
+        public Character leftSpeaker, rightSpeaker;
+        public Line[] lines;
+        
+    }
+}
---
+++ Action - Aventure/Assets/Scripts/Game Management/GameCanvasManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Management;
+using Dialog;
+
+namespace GameManagement
+{
+    public class GameCanvasManager : Singleton<GameCanvasManager>
+    {
+
+        public DialogDisplay dialog = null;
+
+        void Awake()
+        {
+            MakeSingleton(false);
+        }
+    }
+}
