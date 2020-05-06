e0cf84a46a13c79df546c48f2c213b25645a0e48
Charles Bentein <c.bentein@rubika-edu.com>
2020-05-06 19:56:36 +0200

    Added AudioManager

---
+++ Action - Aventure/Assets/Scripts/Sound/AudioManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+using Management;
+using System;
+
+namespace GameSound
+{
+    public class AudioManager : Singleton<AudioManager>
+	{
+        #region Variables
+        public Sound[] sounds;
+        #endregion
+        private void Awake()
+        {
+            MakeSingleton(true);
+
+            foreach(Sound s in sounds)
+            {
+                GetSound(s);
+            }
+        }
+
+        void GetSound(Sound s)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+        public void Play(string name)
+        {
+            Sound s2p = Array.Find(sounds, sound => sound.name == name);
+
+            if(s2p == null)
+            {
+                Debug.LogWarning("Sound: " + name + " not found!");
+                return;
+            }
+
+            s2p.source.Play();
+        }
+    }
+}
+
---
+++ Action - Aventure/Assets/Scripts/Sound/Sound.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+namespace GameSound
+{
+    [System.Serializable]
+    public class Sound : MonoBehaviour
+	{
+        #region Variables
+        public new string name;
+        public AudioClip clip;
+
+        [Range(0f, 1f)]
+        public float volume;
+        [Range(0.1f, 3f)]
+        public float pitch;
+
+        public bool loop;
+
+        [HideInInspector]
+        public AudioSource source;
+        #endregion
+	}
+}
+
