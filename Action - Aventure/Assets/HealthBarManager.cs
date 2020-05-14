a5a216fb9a06bd87e045a6d30fe4e519202eb254
Xanthin <x.paris@rubika-edu.com>
2020-05-14 21:11:41 +0200

    Ajout UI (Manque barre rechargement Incandescence)

---
+++ Action - Aventure/Assets/HealthBarManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player;
+using UnityEngine.UI;
+
+
+public class HealthBarManager : MonoBehaviour
+{
+    [SerializeField]
+    private Image healthBar;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //passer current HP en Float car fill amout [0;1]
+        healthBar.fillAmount = (float)PlayerManager.Instance.currentHp / (float)PlayerManager.Instance.currentMaxHp;
+    }
+}
---
+++ Action - Aventure/Assets/LightCanvasManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Lantern;
+
+public class LightCanvasManager : MonoBehaviour
+{
+    /// <summary>
+    /// Script managing the LanternState in UI.
+    /// </summary>
+
+    [SerializeField]
+    private Image lanternIsLit;
+    [SerializeField]
+    private Image lanternNotlit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lanternIsLit.enabled = true;
+        lanternNotlit.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(LanternManager.Instance.hideLight.currentLightState == lightState.Displayed)
+        {
+            lanternIsLit.enabled = true;
+            lanternNotlit.enabled = false;
+        }
+        else if (LanternManager.Instance.hideLight.currentLightState == lightState.Hidden)
+        {
+            lanternIsLit.enabled = false;
+            lanternNotlit.enabled = true;
+        }
+    }
+}
