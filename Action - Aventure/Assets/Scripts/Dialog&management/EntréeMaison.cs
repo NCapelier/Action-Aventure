4566bcff702910964637b8a7e5234c62cea8ee21
Xanthin <x.paris@rubika-edu.com>
2020-05-24 22:18:37 +0200

    Fin Cut Scene Cave

---
+++ Action - Aventure/Assets/Scripts/Dialog&management/EntréeMaison.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameManagement;
+
+using Player;
+using UnityEngine.Playables;
+
+public class EntréeMaison : MonoBehaviour
+{
+    [SerializeField] public Dialog.Conversation entréeMaison;
+    [SerializeField] public Dialog.Conversation arrivéeMaman;
+
+    [SerializeField] private GameObject enemyEyes;
+    [SerializeField] private GameObject enemySkin;
+
+    [SerializeField] private BoxCollider2D boxCol;
+
+    private PlayableDirector timeline;
+    private bool timelineF = false;
+    private bool playerDial = false;
+
+    private void Start()
+    {
+        timeline = GetComponent<PlayableDirector>();
+        boxCol = GetComponent<BoxCollider2D>();
+    }
+
+private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "PlayerController")
+        {
+
+            StartCoroutine("CutScene");
+            
+
+        }   
+    }
+
+    private void Update()
+    {
+        if(playerDial == true)
+        {
+            PlayerManager.Instance.controller.isDialoging = true;
+        }
+
+        if (timelineF == true)
+        {
+            enemyEyes.SetActive(true);
+            enemySkin.SetActive(true);
+        }
+    }
+
+    IEnumerator CutScene()
+    {
+        GameCanvasManager.Instance.dialog.isCutScene = true;
+        yield return new WaitForSeconds(0.1f);
+        GameCanvasManager.Instance.dialog.StartDialog = entréeMaison;
+        yield return new WaitForSeconds(3.2f);
+        GameCanvasManager.Instance.dialog.forceUpdate = true;
+        yield return new WaitForSeconds(3.2f);
+        GameCanvasManager.Instance.dialog.forceUpdate = true;
+        playerDial = true;
+        yield return new WaitForSeconds(3.2f);
+        GameCanvasManager.Instance.dialog.forceUpdate = true;
+        PlayerManager.Instance.controller.isDialoging = true;
+        yield return new WaitForSeconds(2f);
+       
+        GameCanvasManager.Instance.dialog.isCutScene = true;
+        yield return new WaitForSeconds(0.1f);
+        GameCanvasManager.Instance.dialog.StartDialog = arrivéeMaman;
+        yield return new WaitForSeconds(2f);
+        GameCanvasManager.Instance.dialog.forceUpdate = true;
+        yield return new WaitForSeconds(2f);
+        GameCanvasManager.Instance.dialog.forceUpdate = true;
+        PlayerManager.Instance.controller.isDialoging = false;
+        playerDial = false;
+
+        //timeline.Stop();
+        GameManager.Instance.GetComponent<GameState>().versatileGet = true;
+        timelineF = true;
+
+        boxCol.enabled = false;
+
+
+    }
+}
