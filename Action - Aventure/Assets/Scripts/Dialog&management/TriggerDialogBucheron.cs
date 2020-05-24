8ffbefa48ec4ffffa7c7f6c16769f31f64263f72
Xanthin <x.paris@rubika-edu.com>
2020-05-24 14:00:15 +0200

    Clean Codes (part1)

--- Action - Aventure/Assets/Scripts/Dialog&management/TriggerDialogBucheron.cs
+++ Action - Aventure/Assets/Scripts/Dialog&management/TriggerDialogBucheron.cs
@@ -3,35 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using Player;
 using GameManagement;
+using UnityEngine.Playables;
+
 
 
 public class TriggerDialogBucheron : MonoBehaviour
 {
+    public GameObject enemy;
 
-    private BoxCollider2D bxColiider;
+    public GameObject waypoints;
+
+
+    private BoxCollider2D bxCollider;
 
     public Dialog.Conversation dialHelp1;
     public Dialog.Conversation dialHelp2;
+    public Dialog.Conversation dialHelp3;
     private bool dialog1Finished;
 
     public GameObject flambeau;
 
     private bool playerHere;
 
+    private PlayableDirector timeline;
+
+    public float timerClip = 0f;
+
+    private bool starTimer = false;
+
+    private bool dialog2fin = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        bxColiider = GetComponent<BoxCollider2D>();
+
+
+        bxCollider = GetComponent<BoxCollider2D>();
         flambeau.GetComponent<TorchTTK>().isLit = true;
-        playerHere = false;
-        dialog1Finished = false;
-    }
 
-    private void Update()
-    {
-        
+        timeline = GetComponent<PlayableDirector>();
 
-       
+        timeline.Stop();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -39,9 +52,68 @@ public class TriggerDialogBucheron : MonoBehaviour
         {
             playerHere = true;
 
+            bxCollider.enabled = false;
+
+            PlayerManager.Instance.controller.isDialoging = true;
+
+            GameCanvasManager.Instance.dialog.StartDialog = dialHelp1;
+
+
+        }
+
+
+
+    }
+
+    private void Update()
+    {
+        if (GameCanvasManager.Instance.dialog.runningConversation == false && playerHere == true)
+        {
+            PlayerManager.Instance.controller.isDialoging = true;
+
+            timeline.Play();
+
+            starTimer = true;
+            
+
+            
+        }
+
+
+        if (timerClip >= 1150 && starTimer == true)
+        {
+            starTimer = false;
+
+            GameCanvasManager.Instance.dialog.StartDialog = dialHelp2;
+
+            //jouer coup de feu;
+
+            timeline.Stop();
+
+            flambeau.GetComponent<TorchTTK>().isLit = false;
+
+            dialog2fin = true;
+
+            if (GameCanvasManager.Instance.dialog.runningConversation == false && dialog2fin == true)
+            {
+                Debug.Log("called");
+                PlayerManager.Instance.controller.isDialoging = true;
+
+
+                GameCanvasManager.Instance.dialog.StartDialog = dialHelp3;
+            }
+        }
+
+        
+
+       
+
+
+
+        if (starTimer == true)
+        {
+            timerClip += 1;
         }
     }
 
-
-    
 }
