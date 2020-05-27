221850df2f3ca5b61fc6467f204acbf0702d7e2c
Xanthin <x.paris@rubika-edu.com>
2020-05-27 14:05:39 +0200

    Starting Cut Scene + Posing Camera on dungeon room

---
+++ Action - Aventure/Assets/CutSceneChapel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using GameManagement;
+using Player;
+
+
+public class CutSceneChapel : MonoBehaviour
+{
+    private PlayableDirector timeline;
+
+    private BoxCollider2D boxCol;
+
+    private bool startTimeline;
+    private float timeTimeline;
+
+    private bool finished;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        finished = false;
+        boxCol = GetComponent<BoxCollider2D>();
+        timeline = GetComponent<PlayableDirector>();
+    }
+
+    IEnumerator CutScene()
+    {
+        timeline.Play();
+        yield return null;
+    }
+}
