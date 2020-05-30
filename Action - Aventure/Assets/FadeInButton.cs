eee4a770404c4dcf8f6725e06cd1816821735901
Xanthin <x.paris@rubika-edu.com>
2020-05-30 18:56:57 +0200

    Ajout Shop

---
+++ Action - Aventure/Assets/FadeInButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInButton : MonoBehaviour
+{
+    //Button reference
+    public GameObject AButton;
+    private SpriteRenderer boutonRenderer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startFadingIN()
+    {
+        StartCoroutine("FadeIn");
+    }
+
+    public void startFadingOUT()
+    {
+        StartCoroutine("FadeOut");
+    }
+}
