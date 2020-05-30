27a033c2f0cbe5625975c7c29b1aa893f40a4ffd
Xanthin <x.paris@rubika-edu.com>
2020-05-30 19:33:17 +0200

    Vanishing Ennemy if Dead already in cave & chapelle

---
+++ Action - Aventure/Assets/updateMonstreChapel2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Enemy;
+using GameManagement;
+
+public class updateMonstreChapel2 : MonoBehaviour
+{
+    private EnemyParent enemyParent2;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        enemyParent2 = GetComponent<EnemyParent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyParent2.hp <= 0)
+        {
+            GameManager.Instance.gameState.monstre2chapelle = true;
+
+        }
+
+        if (GameManager.Instance.gameState.monstre2chapelle == true)
+        {
+            gameObject.SetActive(false);
+        }
+
+    }
+}
+
---
+++ Action - Aventure/Assets/updateMonstreChapel3.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Enemy;
+using GameManagement;
+
+public class updateMonstreChapel3 : MonoBehaviour
+{
+    private EnemyParent enemyParent;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        enemyParent = GetComponent<EnemyParent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyParent.hp <= 0)
+        {
+            GameManager.Instance.gameState.monstre3chapelle = true;
+
+        }
+
+        if (GameManager.Instance.gameState.monstre3chapelle == true)
+        {
+            gameObject.SetActive(false);
+        }
+
+    }
+}
