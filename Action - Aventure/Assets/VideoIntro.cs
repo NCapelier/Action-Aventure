48a69e2bd97c343c07853a047d1eb5ef6f11e6df
Xanthin <x.paris@rubika-edu.com>
2020-05-30 15:40:38 +0200

    Script préparé pour Vidéo

--- Action - Aventure/Assets/VideoIntro.cs
+++ Action - Aventure/Assets/VideoIntro.cs
@@ -1,14 +1,24 @@
 ﻿using GameManagement;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using Player;
 using UnityEngine.Video;
 
 public class VideoIntro : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject gameCanvas;
+    private bool startTimer = false;
+    public int videoTime;
+    [SerializeField] private VideoPlayer videoPlayer;
+
+    private void Awake()
     {
-        
+        gameCanvas.SetActive(false);
+
+        startTimer = true;
+
+        videoPlayer.Play();
     }
 
     // Update is called once per frame
@@ -18,7 +28,37 @@ public class VideoIntro : MonoBehaviour
         {
             PlayerManager.Instance.controller.isDialoging = true;
 
-            //LANCER LA VIDEO
+            
         }
+
+        if(videoTime >= 105)
+        {
+            GameManager.Instance.gameState.videoIntroDone = true;
+            videoPlayer.Stop();
+            PlayerManager.Instance.controller.isDialoging = false;
+
+            gameCanvas.SetActive(true);
+        }
+
+        if(startTimer == true)
+        {
+            startTimer = false;
+            StartCoroutine("Timer");
+            
+        }
+                
     }
+
+    IEnumerator Timer()
+    {
+        videoTime++;
+
+        yield return new WaitForSeconds(0.5f);
+        if(GameManager.Instance.gameState.videoIntroDone == false)
+        {
+            StartCoroutine("Timer");
+        }
+      
+    }
+    
 }
