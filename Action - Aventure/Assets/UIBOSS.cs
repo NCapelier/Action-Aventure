053adc5d6805777b8ebf81126eb3c08021e98c7a
Xanthin <x.paris@rubika-edu.com>
2020-05-30 20:46:59 +0200

    Ui boss Finie

--- Action - Aventure/Assets/UIBOSS.cs
+++ Action - Aventure/Assets/UIBOSS.cs
@@ -9,11 +9,41 @@ public class UIBOSS : MonoBehaviour
 
     [SerializeField]
     private Image bossHealthBar;
-   
+
+    public GameObject bossBandeau1;
+
+    public GameObject bossBandeau2;
+
+    public GameObject bossBandeau3;
+
+    
+
 
     // Update is called once per frame
     void Update()
     {
-        bossHealthBar.fillAmount = (float)BossManager.Instance.hp /(float)BossManager.Instance.maxHp;
+      bossHealthBar.fillAmount = (float)BossManager.Instance.hp /(float)BossManager.Instance.maxHp;
+
+
+        if(BossManager.Instance.controller.headBandCount == 3)
+        {
+            bossBandeau1.SetActive(true);
+            bossBandeau2.SetActive(true);
+            bossBandeau3.SetActive(true);
+        }
+
+        if (BossManager.Instance.controller.headBandCount == 2)
+        {
+            bossBandeau3.SetActive(false);
+        }
+        if (BossManager.Instance.controller.headBandCount == 1)
+        {
+            bossBandeau2.SetActive(false);
+        }
+        if (BossManager.Instance.controller.headBandCount == 0)
+        {
+            bossBandeau1.SetActive(false);
+        }
+
     }
 }
