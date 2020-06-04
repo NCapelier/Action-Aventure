a56f3bfd2c00c44676f37dfa36dd6f0e2154ac95
Xanthin <x.paris@rubika-edu.com>
2020-06-04 22:51:48 +0200

    Ajout d'options (Début) : Volume (Need AudioMixer), FullScreen et Résolution

--- Action - Aventure/Assets/Scripts/UI/OptionsMenu.cs
+++ Action - Aventure/Assets/Scripts/UI/OptionsMenu.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class OptionsMenu : MonoBehaviour
 {
+    //[SerializeField] private AudioMixer audioMixer;
 
     [SerializeField] private GameObject playButton;
     [SerializeField] private GameObject shortCutImage;
 
     [SerializeField] private GameObject pauseButtonReturn;
+
+    private void Start()
+    {
+        //audioMixer = GameSound.AudioManager.Instance.GetComponent<AudioMixer>();
+    }
 
     private void Update()
     {
@@ -35,4 +42,24 @@ public class OptionsMenu : MonoBehaviour
         Debug.Log("Quitter");
         AppHelper.Quit();
     }
+
+    public void FullScreen(bool isFullScreen)
+    {
+
+        Screen.fullScreen = isFullScreen;
+        
+        
+    }
+
+    public void SetVolume (float volume)
+    {
+        //Need un Audio Mixer pour que cela marche
+        //audioMixer.SetFloat("volume");
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
 }
