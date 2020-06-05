da41dbce745013607f09bbe25083526b67cf1653
Xanthin <x.paris@rubika-edu.com>
2020-06-05 16:28:43 +0200

    Debut cutscene Boss1

--- Action - Aventure/Assets/UIBOSS.cs
+++ Action - Aventure/Assets/UIBOSS.cs
@@ -16,6 +16,8 @@ public class UIBOSS : MonoBehaviour
 
     public GameObject bossBandeau3;
 
+    [SerializeField] private GameObject allObjects;
+
     
 
 
@@ -24,26 +26,61 @@ public class UIBOSS : MonoBehaviour
     {
       bossHealthBar.fillAmount = (float)BossManager.Instance.hp /(float)BossManager.Instance.maxHp;
 
-
-        if(BossManager.Instance.controller.headBandCount == 3)
+        if (BossManager.Instance.controller.currentBossState == bossState.CutScene1)
         {
-            bossBandeau1.SetActive(true);
-            bossBandeau2.SetActive(true);
-            bossBandeau3.SetActive(true);
-        }
-
-        if (BossManager.Instance.controller.headBandCount == 2)
-        {
+            bossHealthBar.enabled = false;
+            bossBandeau1.SetActive(false);
+            bossBandeau2.SetActive(false);
             bossBandeau3.SetActive(false);
         }
-        if (BossManager.Instance.controller.headBandCount == 1)
+
+       else if (BossManager.Instance.controller.currentBossState == bossState.Phase1)
         {
-            bossBandeau2.SetActive(false);
+            if (BossManager.Instance.controller.headBandCount == 3)
+            {
+                bossHealthBar.enabled = true;
+                bossBandeau1.SetActive(true);
+                bossBandeau2.SetActive(true);
+                bossBandeau3.SetActive(true);
+            }
+
+            if (BossManager.Instance.controller.headBandCount == 2)
+            {
+                bossBandeau3.SetActive(false);
+            }
+            if (BossManager.Instance.controller.headBandCount == 1)
+            {
+                bossBandeau2.SetActive(false);
+            }
+            if (BossManager.Instance.controller.headBandCount == 0)
+            {
+                bossBandeau1.SetActive(false);
+            }
         }
-        if (BossManager.Instance.controller.headBandCount == 0)
+
+        else if (BossManager.Instance.controller.currentBossState == bossState.CutScene2)
         {
+            bossHealthBar.enabled = false;
+           
+        }
+
+        else if (BossManager.Instance.controller.currentBossState == bossState.Phase2)
+        {
+            bossHealthBar.enabled = true;
+        }
+
+
+        else if (BossManager.Instance.controller.currentBossState == bossState.CutScene3)
+        {
+            bossHealthBar.enabled = false;
             bossBandeau1.SetActive(false);
+            bossBandeau2.SetActive(false);
+            bossBandeau3.SetActive(false);
         }
+
+
+
+
 
     }
 }
