77a32707e56fa172df80bdf5cfc5e4288bb47a7e
Charles Bentein <c.bentein@rubika-edu.com>
2020-06-08 17:20:20 +0200

    load threshold for attack sound change augmented

--- Action - Aventure/Assets/Scripts/Player/PlayerContactAttack.cs
+++ Action - Aventure/Assets/Scripts/Player/PlayerContactAttack.cs
@@ -138,7 +138,7 @@ namespace Player
 
         void PlayAttackSound()
         {
-            if(loading < maxLoad * 0.15f)
+            if(loading < maxLoad * 0.30f)
             {
                 AudioManager.Instance.Play("Lantern_light_strike");
             }
