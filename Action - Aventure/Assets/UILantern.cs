b1b3044e2c210dd1fe0eac56cdf152fcd81a5b00
Xanthin <x.paris@rubika-edu.com>
2020-06-13 18:23:14 +0200

    UIFIX

---
+++ Action - Aventure/Assets/UILantern.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lantern;
+using UnityEngine.UI;
+
+public class UILantern : MonoBehaviour
+{
+
+    [SerializeField]
+    private Image lanternBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lanternBar.fillAmount = LanternManager.Instance.hideLight.displayTime;
+    }
+}
